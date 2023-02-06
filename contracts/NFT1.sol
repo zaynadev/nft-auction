f67fc0ed2118609c04e3ab9dd24bf3bf8ec3c0cc
Zainab Oufqir <zayna.oufkir@gmail.com>
2023-02-06 16:26:15 +0100

    add nft1 and nft2 contract

--- contracts/NFT1.sol
+++ contracts/NFT1.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
-contract NFT1 {}
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT1 is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFT1", "N1") {}
+
+    function _mintToken(address _to, string calldate _uri) public returns (uint) {
+        _tokenIds.increment();
+        uint id = _tokenIds.current();
+        _safeMint(_to, id);
+        _setTokenURI(id, _uri);
+        return id;
+    }
+}
--- contracts/NFT2.sol
+++ contracts/NFT2.sol
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
-contract NFT2 {}
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT2 is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFT2", "N2") {}
+
+    function _minToken(
+        address _to,
+        string calldata _uri
+    ) public returns (uint) {
+        _tokenIds.increment();
+        uint id = _tokenIds.current();
+        _safeMint(_to, id);
+        _setTokenURI(id, _uri);
+        return id;
+    }
+}
